a9922f81a5d3a9345e3d2852a32c151dc8cf42ac
zhaohuimin <zhaohuimin@wmeimob.com>
2021-02-14 08:02:49 +0800

    排序与搜索算法

--- 10-tree/BinarySearchTree.js
+++ 10-tree/BinarySearchTree.js
@@ -8,4 +8,44 @@ export default class BinarySearchTree {
         this.compareFn = compareFn;
         this.root = null;
     }
+    insert(key) {
+        if(this.root == null) {
+            this.root = new Node(key);
+        }else {
+            this.insertNode(this.root, key)
+        }
+    }
+    insertNode(node,key) {
+        if(this.compareFn(key,node.key) === Compare.LESS_THAN) {
+            if(node.left == null) {
+                node.left = new Node(key);
+            } else {
+                this.insertNode(node.left,key)
+            }
+        } else {
+            if(node.right == null) {
+                node.right = new Node(key);
+            }else {
+                this.insertNode(node,right,key);
+            }
+        }
+    }
+
+    inOrderTraverse(callback) {
+        this.inOrderTraverseNode(this.root, callback);
+    }
+    inOrderTraverseNode(node, callback) {
+        if(node != null) {
+            this.inOrderTraverseNode(node.left, callback);
+            callback(node.key);
+            this.inOrderTraverseNode(node.right, callback);
+        }
+    }
 }
+
+const tree = new BinarySearchTree();
+tree.insert(11);
+
+console.log(tree.inOrderTraverse())
+
+
